df4fb3f5ee654ede93855089978e787db1484909
kafeleababa <kafelekadin@gmail.com>
2023-04-17 12:38:24 +0000

    setting up app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import cover from './Switzerland_003.jpg'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h1>Switzerland</h1>
+        <img src={cover} width={500} alt='lake'/>
       </header>
     </div>
   );
