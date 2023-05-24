197629ee7e77237b24acd1e7d4e052404f4efe00
kafeleababa <kafelekadin@gmail.com>
2023-05-24 00:41:37 +0000

    resize switzerland photos

--- src/albums/Switzerland/imageGallery.js
+++ src/albums/Switzerland/imageGallery.js
@@ -1,274 +1,269 @@
-import S01 from './images/Switzerland_001.jpeg';
-import S02 from './images/Switzerland_002.jpeg';
-import S03 from './images/Switzerland_003.jpeg';
-import S04 from './images/Switzerland_004.jpeg';
-import S05 from './images/Switzerland_005.jpeg';
-import S06 from './images/Switzerland_006.jpeg';
-import S07 from './images/Switzerland_007.jpeg';
-import S08 from './images/Switzerland_008.jpeg';
-import S09 from './images/Switzerland_009.jpeg';
-import S10 from './images/Switzerland_010.jpeg';
-import S11 from './images/Switzerland_011.jpeg';
-import S12 from './images/Switzerland_012.jpeg';
-import S13 from './images/Switzerland_013.jpeg';
-import S14 from './images/Switzerland_014.jpeg';
-import S15 from './images/Switzerland_015.jpeg';
-import S16 from './images/Switzerland_016.jpeg';
-import S17 from './images/Switzerland_017.jpeg';
-import S18 from './images/Switzerland_018.jpeg';
-import S19 from './images/Switzerland_019.jpeg';
-import S20 from './images/Switzerland_020.jpeg';
-import S21 from './images/Switzerland_021.jpeg';
-import S22 from './images/Switzerland_022.jpeg';
-import S23 from './images/Switzerland_023.jpeg';
-import S24 from './images/Switzerland_024.jpeg';
-import S25 from './images/Switzerland_025.jpeg';
-import S26 from './images/Switzerland_026.jpeg';
-import S27 from './images/Switzerland_027.jpeg';
-import S28 from './images/Switzerland_028.jpeg';
-import S29 from './images/Switzerland_029.jpeg';
-import S30 from './images/Switzerland_030.jpeg';
-import S31 from './images/Switzerland_031.jpeg';
-import S32 from './images/Switzerland_032.jpeg';
-import S33 from './images/Switzerland_033.jpeg';
-import S34 from './images/Switzerland_034.jpeg';
-import S35 from './images/Switzerland_035.jpeg';
-import S36 from './images/Switzerland_036.jpeg';
-import S37 from './images/Switzerland_037.jpeg';
-import S38 from './images/Switzerland_038.jpeg';
-import S39 from './images/Switzerland_039.jpeg';
-import S40 from './images/Switzerland_040.jpeg';
-import S41 from './images/Switzerland_041.jpeg';
-import S42 from './images/Switzerland_042.jpeg';
-import S43 from './images/Switzerland_043.jpeg';
-import S44 from './images/Switzerland_044.jpeg';
-import S45 from './images/Switzerland_045.jpeg';
+import S01 from './images/Switzerland_001.jpg';
+import S02 from './images/Switzerland_002.jpg';
+import S03 from './images/Switzerland_003.jpg';
+import S04 from './images/Switzerland_004.jpg';
+import S05 from './images/Switzerland_005.jpg';
+import S06 from './images/Switzerland_006.jpg';
+import S07 from './images/Switzerland_007.jpg';
+import S08 from './images/Switzerland_008.jpg';
+import S09 from './images/Switzerland_009.jpg';
+import S10 from './images/Switzerland_010.jpg';
+import S11 from './images/Switzerland_011.jpg';
+import S12 from './images/Switzerland_012.jpg';
+import S13 from './images/Switzerland_013.jpg';
+import S14 from './images/Switzerland_014.jpg';
+import S15 from './images/Switzerland_015.jpg';
+import S16 from './images/Switzerland_016.jpg';
+import S17 from './images/Switzerland_017.jpg';
+import S18 from './images/Switzerland_018.jpg';
+import S19 from './images/Switzerland_019.jpg';
+import S20 from './images/Switzerland_020.jpg';
+import S21 from './images/Switzerland_021.jpg';
+import S22 from './images/Switzerland_022.jpg';
+import S23 from './images/Switzerland_023.jpg';
+import S24 from './images/Switzerland_024.jpg';
+import S25 from './images/Switzerland_025.jpg';
+import S26 from './images/Switzerland_026.jpg';
+import S27 from './images/Switzerland_027.jpg';
+import S28 from './images/Switzerland_028.jpg';
+import S29 from './images/Switzerland_029.jpg';
+import S30 from './images/Switzerland_030.jpg';
+import S31 from './images/Switzerland_031.jpg';
+import S32 from './images/Switzerland_032.jpg';
+import S33 from './images/Switzerland_033.jpg';
+import S34 from './images/Switzerland_034.jpg';
+import S35 from './images/Switzerland_035.jpg';
+import S36 from './images/Switzerland_036.jpg';
+import S37 from './images/Switzerland_037.jpg';
+import S38 from './images/Switzerland_038.jpg';
+import S39 from './images/Switzerland_039.jpg';
+import S40 from './images/Switzerland_040.jpg';
+import S41 from './images/Switzerland_041.jpg';
+import S42 from './images/Switzerland_042.jpg';
+import S43 from './images/Switzerland_043.jpg';
+import S44 from './images/Switzerland_044.jpg';
+import S45 from './images/Switzerland_045.jpg';
 
 export const switzerland_images = [
     {
         src: S01,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S02,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S03,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S04,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S05,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S06,
-        width: 4,
-        height: 5
+        width: 9,
+        height: 16
     },
     {
         src: S07,
-        width: 4,
-        height: 5
+        width: 9,
+        height: 16
     },
     {
         src: S08,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S09,
-        width: 4,
-        height: 5
+        width: 9,
+        height: 16
     },
     {
         src: S10,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S11,
-        width: 4,
-        height: 5
+        width: 9,
+        height: 16
     },
     {
         src: S12,
-        width: 4,
-        height: 5
+        width: 9,
+        height: 16
     },
     {
         src: S13,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S14,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S15,
-        width: 4,
-        height: 5
+        width: 9,
+        height: 16
     },
     {
         src: S16,
-        width: 4,
-        height: 5
+        width: 9,
+        height: 16
     },
     {
         src: S17,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S18,
-        width: 4,
-        height: 5
-    },
-    {
-        src: S19,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S20,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S21,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S22,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S23,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S24,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S25,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S26,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S27,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S28,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S29,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S30,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S31,
-        width: 4,
-        height: 5
+        width: 5,
+        height: 4
     },
     {
         src: S32,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S33,
-        width: 4,
-        height: 5
+        width: 9,
+        height: 16
     },
     {
         src: S34,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S35,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S36,
-        width: 4,
-        height: 5
+        width: 9,
+        height: 16
     },
     {
         src: S37,
-        width: 4,
-        height: 5
+        width: 9,
+        height: 16
     },
     {
         src: S38,
-        width: 4,
-        height: 5
+        width: 9,
+        height: 16
     },
     {
         src: S39,
-        width: 4,
-        height: 5
+        width: 9,
+        height: 16
     },
     {
         src: S40,
-        width: 4,
-        height: 5
+        width: 9,
+        height: 16
     },
     {
         src: S41,
-        width: 4,
-        height: 5
+        width: 9,
+        height: 16
     },
     {
         src: S42,
-        width: 4,
-        height: 5
+        width: 9,
+        height: 16
     },
     {
         src: S43,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     },
     {
         src: S44,
-        width: 4,
-        height: 5
+        width: 5,
+        height: 4
     },
     {
         src: S45,
-        width: 4,
-        height: 5
+        width: 16,
+        height: 9
     }
 ];
 
